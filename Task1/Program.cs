efe6eee911c25b4a757b78ae923d361b18aaf059
unknown <kovgankosvetlana.com>
2022-01-02 14:50:25 +0300

    task1.3

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,27 +1,27 @@
 ﻿//Написать программу, которая из имеющегося массива целых чисел формирует массив из четных чисел. 
 //Первоначальный массив можно ввести с клавиатуры, либо сгенерировать случайным образом.
-int j=0;
-int [] Array = new int [10];
-int [] EventArray = new int [10];
-void PrintArray (int [] Array)  // Создание и распечатывание массива
+int j = 0;
+int[] Array = new int[10];
+int[] EventArray = new int[10];
+void PrintArray(int[] Array)  // Создание и распечатывание массива
 {
-    for(int i=0; i<Array.Length; i++)
+    for (int i = 0; i < Array.Length; i++)
     {
-        Array[i]=new Random().Next(-50,50);
+        Array[i] = new Random().Next(-50, 50);
         Console.Write($"{Array[i]} ");
     }
 }
-void GetArray (int [] EvenArray)
+void GetArray(int[] EvenArray) // Создание массива на основе другого с четными числами 
 {
-    for(int i=0; i<Array.Length; i++)
+    for (int i = 0; i < Array.Length; i++)
     {
-        if(Array[i]%2==0)
+        if (Array[i] % 2 == 0)
         {
-            EvenArray[j]=Array[i];
+            EvenArray[j] = Array[i];
             Console.Write($"{EvenArray[j]} ");
             j++;
         }
-        
+
     }
 }
 Console.WriteLine("Массив из целых чисел");
